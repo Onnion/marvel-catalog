b400f7eae84df48933dd4eda13f5b6314a794c30
Onnion <marcio.hsdl@gmail.com>
2020-08-29 19:59:36 -0300

    fix codeclimate duplications[2]

--- src/components/ComicCreator/ComicCreator.test.tsx
+++ src/components/ComicCreator/ComicCreator.test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { ComicCreator } from '.';
+import { ComicCreator as ComicCreatorComponent } from '.';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 describe('<ComicCreator /> unit test', () => {
     it('should render without error', () => {
-        const Comic = (props: any) => <ComicCreator {...props} />
+        const Comic = (props: any) => <ComicCreatorComponent {...props} />
         const component = shallow(<Comic />);
         expect(component).toMatchSnapshot();
     });
