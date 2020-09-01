0f7cca89ba8350d286465774a54c898d99ecf185
Onnion <marcio.hsdl@gmail.com>
2020-09-01 10:41:54 -0300

    change README.MD

---
+++ env.exemple.ts
@@ -0,0 +1,6 @@
+export const PUBLIC_KEY = '';
+export const PRIVATE_KEY = '';
+export const API_URL = 'https://gateway.marvel.com/v1/public';
+
+export const RAPIAPI_URL = '';
+export const RAPIAPI_KEY = '';
