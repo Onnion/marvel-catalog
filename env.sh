531f10c24d78301a53666d8cce912c91efabc49d
Onnion <marcio.hsdl@gmail.com>
2020-09-01 14:58:27 -0300

    setup travis

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 mkdir $TRAVIS_BUILD_DIR/src/config
-cp -f  $TRAVIS_BUILD_DIR/env.exemple.ts  $TRAVIS_BUILD_DIR/src/config/env.ts
+cp -f  $TRAVIS_BUILD_DIR/env.exemple.ts  $TRAVIS_BUILD_DIR/src/configs/env.ts
